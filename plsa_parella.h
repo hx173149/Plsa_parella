886753c66c55cd59cd794260bab228a7f6edce87
HouXin <hx173149@gmail.com>
2015-06-17 17:31:28 +0800

    test

---
+++ plsa_parella.h
@@ -0,0 +1,42 @@
+#define MAX_DOC_NUM_ 50000 
+#define MAX_WORD_NUM_ 100000
+#define MAX_TOPIC_NUM_ 1000 
+#define THREAD_NUM 4
+
+#include <map>
+
+using namespace std;
+
+class PLSAOBJParella
+{
+  private:
+    int doc_num;
+    int topic_num;
+    int word_num;
+
+    float** pt_d;
+    float** pw_t;
+
+    double** in_train_data;
+    double m_likehood;
+
+    map<int,float*>  pt_wd;
+
+    void EStep(int start_doc,int end_doc);
+    void MStep(int start_topic,int end_topic);
+    void LogLikehood(int start_doc,int end_doc);
+  
+  public:
+    PLSAOBJParella();
+    ~PLSAOBJParella();
+    int TrainModel(double** train_data,int d_num,int w_num,int t_num,double eps,int max_iter);
+    int ContinueTrain(double** train_data,int d_num,int w_num,int t_num,int max_iter);
+    int ReduceDimension(double* src,double* dst,int& len,int iter_max);
+    int SaveModel(const string& filepath);
+    int LoadModel(const string& filepath);
+  //protected:
+  public:
+    int GetSize(int& d_num,int& t_num,int& w_num);
+    float** GetPT_D();
+    float** GetPW_T() {return pw_t;};
+};
